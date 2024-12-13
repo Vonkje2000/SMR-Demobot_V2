53e1bc2d72b98181047672a9d4e71b218f4345f6
Anton <antonvonk2000@gmail.com>
2024-12-13 12:04:44 +0100

    Python classes for the kawasaki arms work

--- Kawasaki_class.py
+++ Kawasaki_class.py
@@ -1,3 +1,5 @@
+import socket
+
 class Singleton(type):
 	_instances = {}
 	def __call__(cls, *args, **kwargs):
@@ -6,17 +8,24 @@ class Singleton(type):
 		return cls._instances[cls]
 
 class Kawasaki_arm(object):
-	def __init__(self, ip):
+	def __init__(self, ip:str, port:int):
+		if not isinstance(ip, str):
+			raise TypeError("ip must be a string")
+		if not isinstance(port, int):
+			raise TypeError("port must be an int")
 		self.ip = ip
+		self.port = port
+		self.socket_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.__open_Connection()
 
 	def printIP(self):
 		print(self.ip)
 
 	def HOME(self):
-		print("HOME")
+		self.__send_to_arm("HOME")
 
 	def HOME2(self):
-		print("HOME 2")
+		self.__send_to_arm("HOME 2")
 
 	def SPEED(self, SPEED:float|int):
 		if not isinstance(SPEED, float|int):
@@ -26,7 +35,7 @@ class Kawasaki_arm(object):
 			raise ValueError("SPEED value to LOW minimum value 1")
 		if SPEED > 100:
 			raise ValueError("SPEED value to HIGH maximum value 100")
-		print("SPEED {0} ALWAYS".format(SPEED))
+		self.__send_to_arm("SPEED {0} ALWAYS".format(SPEED))
 
 	def JMOVE(self, JT0:float|int, JT1:float|int, JT2:float|int, JT3:float|int, JT4:float|int, JT5:float|int):
 		if not isinstance(JT0, float|int):
@@ -47,7 +56,7 @@ class Kawasaki_arm(object):
 		if not isinstance(JT5, float|int):
 			raise TypeError("JT5 must be a float or an int")
 		JT5 = round(JT5, 2)
-		print("JMOVE ({0}, {1}, {2}, {3}, {4}, {5})".format(JT0, JT1, JT2, JT3, JT4, JT5))
+		self.__send_to_arm("JMOVE ({0}, {1}, {2}, {3}, {4}, {5})".format(JT0, JT1, JT2, JT3, JT4, JT5))
 
 	def JMOVE_TRANS(self, X:float|int, Y:float|int, Z:float|int, O:float|int, A:float|int, T:float|int):
 		if not isinstance(X, float|int):
@@ -68,7 +77,7 @@ class Kawasaki_arm(object):
 		if not isinstance(T, float|int):
 			raise TypeError("T must be a float or an int")
 		T = round(T, 2)
-		print("JMOVE TRANS ({0}, {1}, {2}, {3}, {4}, {5})".format(X, Y, Z, O, A, T))
+		self.__send_to_arm("JMOVE TRANS ({0}, {1}, {2}, {3}, {4}, {5})".format(X, Y, Z, O, A, T))
 
 	def LMOVE(self, JT0:float|int, JT1:float|int, JT2:float|int, JT3:float|int, JT4:float|int, JT5:float|int):
 		if not isinstance(JT0, float|int):
@@ -89,7 +98,7 @@ class Kawasaki_arm(object):
 		if not isinstance(JT5, float|int):
 			raise TypeError("JT5 must be a float or an int")
 		JT5 = round(JT5, 2)
-		print("LMOVE ({0}, {1}, {2}, {3}, {4}, {5})".format(JT0, JT1, JT2, JT3, JT4, JT5))
+		self.__send_to_arm("LMOVE ({0}, {1}, {2}, {3}, {4}, {5})".format(JT0, JT1, JT2, JT3, JT4, JT5))
 
 	def LMOVE_TRANS(self, X:float|int, Y:float|int, Z:float|int, O:float|int, A:float|int, T:float|int):
 		if not isinstance(X, float|int):
@@ -110,7 +119,7 @@ class Kawasaki_arm(object):
 		if not isinstance(T, float|int):
 			raise TypeError("T must be a float or an int")
 		T = round(T, 2)
-		print("LMOVE TRANS ({0}, {1}, {2}, {3}, {4}, {5})".format(X, Y, Z, O, A, T))
+		self.__send_to_arm("LMOVE TRANS ({0}, {1}, {2}, {3}, {4}, {5})".format(X, Y, Z, O, A, T))
 
 	def TOOL(self, X:float|int, Y:float|int, Z:float|int, O:float|int, A:float|int, T:float|int):
 		if not isinstance(X, float|int):
@@ -131,12 +140,34 @@ class Kawasaki_arm(object):
 		if not isinstance(T, float|int):
 			raise TypeError("T must be a float or an int")
 		T = round(T, 2)
-		print("TOOL TRANS ({0}, {1}, {2}, {3}, {4}, {5})".format(X, Y, Z, O, A, T))
+		self.__send_to_arm("TOOL TRANS ({0}, {1}, {2}, {3}, {4}, {5})".format(X, Y, Z, O, A, T))
+
+	def __open_Connection(self):
+		try:
+			self.socket_connection.connect((self.ip, self.port))
+		except:
+			raise ConnectionError("Could not connect to kawasaki robot arm on IP: {0}, Port: {1}".format(self.ip, self.port))
+
+	def __close_Connection(self):
+		self.socket_connection.close()
+
+	def __send_to_arm(self, data:str):
+		if not isinstance(data, str):
+			raise TypeError("ip must be a string")
+		self.__receive_from_arm()
+		self.socket_connection.send(data.encode('utf-8'))
+
+	def __receive_from_arm(self):
+		return self.socket_connection.recv(1024).decode()
+
+	def __del__(self):
+		if(hasattr(socket.socket, 'socket_connection')):
+			self.__close_Connection()
 
 class Kawasaki_1(Kawasaki_arm, metaclass=Singleton):
 	def __init__(self):
-		super().__init__("192.168.0.3")
+		super().__init__("192.168.0.1", 42069)
 
 class Kawasaki_2(Kawasaki_arm, metaclass=Singleton):
 	def __init__(self):
-		super().__init__("192.168.0.1")
+		super().__init__("192.168.0.3", 42069)
--- TCP_Communication_Kawasaki_demo.py
+++ TCP_Communication_Kawasaki_demo.py
@@ -7,9 +7,26 @@ k2 = Kawasaki_2()
 k1.printIP()
 k2.printIP()
 
+k1.SPEED(100)
+k2.SPEED(45)
+
+k1.TOOL(0, 0, 0, 0, 0, 0)
+k2.TOOL(0, 0, 0, 0, 0, 0)
+
 k1.HOME()
-k1.JMOVE_TRANS(-1.0, 2.0, 3.0, 4.0, 5.0, 6.0)
+k2.HOME()
 
-k1.JMOVE(0, 5.0, -6.0, 7.0, 4.0, 2.0)
+k1.JMOVE(4, -15, -125, 9, 22, 170)
+k2.JMOVE(4, -15, -125, 9, 22, 170)
 
-k1.SPEED(1)
+k1.JMOVE_TRANS(20, 370, 150, 90, 90, 90)
+k2.JMOVE_TRANS(20, 370, 150, 90, 90, 90)
+
+k1.JMOVE_TRANS(20, 550, 150, 90, 90, 90)
+k2.JMOVE_TRANS(20, 550, 150, 90, 90, 90)
+
+k1.LMOVE_TRANS(20, 370, 150, 90, 90, 90)
+k2.LMOVE_TRANS(20, 370, 150, 90, 90, 90)
+
+k1.TOOL(40, 30, 60, 0, 0, 0)
+k2.TOOL(40, 30, 60, 0, 0, 0)

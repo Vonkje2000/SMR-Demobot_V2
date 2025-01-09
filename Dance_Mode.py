c3587dd0803dd2221215fb4bcf87cbd63e1636fc
AlphaNova7 <stathis-lefteriotis@hotmail.com>
2025-01-09 17:14:31 +0100

    Begining to program dance_Mode

---
+++ Dance_Mode.py
@@ -0,0 +1,30 @@
+from Promobot_class import Kawasaki_1
+
+k1 = Kawasaki_1()
+
+
+k1.printIP()
+
+
+k1.SPEED(20)
+
+
+k1.TOOL(0, 0, 0, 0, 0, 0)
+
+
+k1.HOME()
+
+
+k1.JMOVE(4, -15, -125, 9, 22, 170)
+
+
+k1.JMOVE_TRANS(20, 370, 150, 90, 90, 90)
+
+
+k1.JMOVE_TRANS(20, 550, 150, 90, 90, 90)
+
+
+k1.LMOVE_TRANS(20, 370, 150, 90, 90, 90)
+
+
+k1.TOOL(40, 30, 60, 0, 0, 0)
